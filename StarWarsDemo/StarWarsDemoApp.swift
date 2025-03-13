392ab7a833552f3ca14395e1dfd8a92f06d0f73a
Eduardo Monroy <edu.wap2@gmail.com>
2025-03-13 11:13:20 +0100

    Initial Commit

---
+++ StarWarsDemo/StarWarsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsDemoApp.swift
+//  StarWarsDemo
+//
+//  Created by Eduardo Monroy Husillos on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
